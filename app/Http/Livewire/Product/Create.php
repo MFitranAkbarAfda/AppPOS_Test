8e42ccc7b8a09a4c0fe5d8c3440c21afed311b95
Akbar <muhammadfitran52@gmail.com>
2022-07-28 23:11:47 +0700

    AppPOS_Test

---
+++ app/Http/Livewire/Product/Create.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Livewire\Product;
+
+use Livewire\Component;
+use App\Models\Product;
+
+class Create extends Component
+{
+
+    public $name_product;
+    public $code_product;
+    public $price;
+
+    protected $rules = [
+        'name_product' => 'required|min:6',
+        'code_product' => 'required',
+        'price' => 'required'
+    ];
+
+    public function submit()
+    {
+        $this->validate();
+
+        $product = Product::create([
+            'name_product' => $this->name_product,
+            'code_product' => $this->code_product,
+            'price' => $this->price
+        ]);
+
+        $this->emit('StoreProduct', $product);
+
+        $this->deleteInput();
+
+    }
+
+    public function deleteInput()
+    {
+        $this->name_product = null;
+        $this->code_product = null;
+        $this->price = null;
+    }
+
+    public function render()
+    {
+        return view('livewire.product.create');
+    }
+}
